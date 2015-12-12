df8af8f0d1eaf40cce62e38e34a223db614dc1a3
Logan Moore <monday.hopscotch@gmail.com>
2015-12-12 10:25:25 -0700

    upgrade menus first run

---
+++ src/main/java/ludum/dare/RacerGame.java
@@ -0,0 +1,24 @@
+package ludum.dare;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureAtlas;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureAtlasLoader;
+import ludum.dare.screens.UpgradeScreen;
+
+/**
+ * Created by Admin on 12/12/2015.
+ */
+public class RacerGame extends Game {
+    public static AssetManager assetManager = new AssetManager();
+    @Override
+    public void create() {
+        assetManager.setLoader(AnimagicTextureAtlas.class, new AnimagicTextureAtlasLoader(new InternalFileHandleResolver()));
+
+        assetManager.load("packed/test.atlas", AnimagicTextureAtlas.class);
+        assetManager.finishLoading();
+
+        setScreen(new UpgradeScreen());
+    }
+}
