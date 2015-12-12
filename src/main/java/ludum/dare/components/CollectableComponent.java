6a42dfd6ef847b79ee1dc21d83c5ac922c532c4f
Jacob Fisher <zombiefish90@gmail.com>
2015-12-12 09:49:21 -0700

    adding item classes and upgrade component classes

---
+++ src/main/java/ludum/dare/components/CollectableComponent.java
@@ -0,0 +1,17 @@
+package ludum.dare.components;
+
+import ludum.dare.interfaces.IComponent;
+import ludum.dare.interfaces.IUpdate;
+
+/**
+ * Created by jake on 12/11/2015.
+ */
+public class CollectableComponent implements IComponent, IUpdate{
+
+    private boolean beenCollected = false;
+
+    @Override
+    public void update(float delta) {
+
+    }
+}
