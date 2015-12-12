cbf61f285e0f82e1b320ed0a79fff0834b28ed73
Michael Wingfield <mwingfie@gmail.com>
2015-12-12 00:45:42 -0700

    more gameobject-y

--- src/main/java/ludum/dare/screens/RaceScreen.java
+++ src/main/java/ludum/dare/screens/RaceScreen.java
@@ -6,9 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.bytebreakstudios.animagic.texture.AnimagicSpriteBatch;
+import ludum.dare.actors.GameObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,23 +17,21 @@ public class RaceScreen implements Screen {
     OrthographicCamera[] cameras = new OrthographicCamera[4];
     AnimagicSpriteBatch batch;
 
-    List<TextureRegion> drawShit = new ArrayList<>();
+    List<GameObject> gameObjects = new ArrayList<>();
 
     @Override
     public void show() {
         for (int i = 0; i < cameras.length; i++) cameras[i] = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight() / cameras.length);
         batch = new AnimagicSpriteBatch();
-
-        drawShit.add(new TextureRegion(new Texture("badlogic.jpg")));
     }
 
     @Override
-    public void render(float v) {
+    public void render(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) cameras[0].translate(-1, 0);
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) cameras[0].translate(1, 0);
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) cameras[0].translate(0, 1);
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) cameras[0].translate(0, -1);
-        update();
+        update(delta);
         for (Camera cam : cameras) cam.update();
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -48,12 +45,12 @@ public class RaceScreen implements Screen {
         }
     }
 
-    public void update(){
-
+    public void update(float delta){
+        gameObjects.forEach(obj -> obj.update(delta));
     }
 
     public void draw(){
-        drawShit.forEach(tex -> batch.draw(tex, Gdx.graphics.getWidth() / -2, Gdx.graphics.getHeight() / -2, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        gameObjects.forEach(obj -> obj.draw(batch));
     }
 
     @Override
