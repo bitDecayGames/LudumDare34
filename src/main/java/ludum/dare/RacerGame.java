5121581d39c1bf39e9a4f7faf28b9085d9e4b11c
Logan Moore <monday.hopscotch@gmail.com>
2015-12-12 12:37:47 -0700

    load assets asynchronously

--- src/main/java/ludum/dare/RacerGame.java
+++ src/main/java/ludum/dare/RacerGame.java
@@ -12,14 +12,14 @@ import ludum.dare.screens.SplashScreen;
  */
 public class RacerGame extends Game {
     public static AssetManager assetManager = new AssetManager();
+    public static void queueAssetsForLoad() {
+        assetManager.setLoader(AnimagicTextureAtlas.class, new AnimagicTextureAtlasLoader(new InternalFileHandleResolver()));
+        RacerGame.assetManager.load("packed/test.atlas", AnimagicTextureAtlas.class);
+        RacerGame.assetManager.load("packed/menu.atlas", AnimagicTextureAtlas.class);
+    }
+
     @Override
     public void create() {
-        assetManager.setLoader(AnimagicTextureAtlas.class, new AnimagicTextureAtlasLoader(new InternalFileHandleResolver()));
-
-        assetManager.load("packed/test.atlas", AnimagicTextureAtlas.class);
-        assetManager.load("packed/menu.atlas", AnimagicTextureAtlas.class);
-        assetManager.finishLoading();
-
         setScreen(new SplashScreen(this));
     }
 }
