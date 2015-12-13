c9b3ff9da3aa47a23ae2f96537969f6b0484609d
Jacob Fisher <zombiefish90@gmail.com>
2015-12-12 22:46:27 -0700

    timed componenet, Iremovable have been added. Game object has been updated to remove removable componentts

--- src/main/java/ludum/dare/components/PowerUpComponents/TempSpeedComponent.java
+++ src/main/java/ludum/dare/components/PowerUpComponents/TempSpeedComponent.java
@@ -1,20 +1,24 @@
 package ludum.dare.components.PowerUpComponents;
 
+import ludum.dare.components.PhysicsComponent;
+import ludum.dare.components.upgradeComponents.TimedComponent;
 import ludum.dare.interfaces.IComponent;
 import ludum.dare.interfaces.IUpdate;
 
 /**
  * Created by jake on 12/12/2015.
  */
-public class TempSpeedComponent implements IComponent, IUpdate{
-    int powerTime = 5;
+public class TempSpeedComponent extends TimedComponent {
+    private PhysicsComponent phys;
 
-    public TempSpeedComponent(){
-
+    public TempSpeedComponent(PhysicsComponent phys){
+        super(5);
+        this.phys = phys;
     }
 
     @Override
     public void update(float delta) {
-
+        super.update(delta);
     }
+
 }
