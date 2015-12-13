3aebdb3f62b8ad1fbdeb9b269b7004f188de7e10
Michael Wingfield <mwingfie@gmail.com>
2015-12-12 19:42:39 -0700

    did a bunch of state stuff

--- src/main/java/ludum/dare/actors/player/Player.java
+++ src/main/java/ludum/dare/actors/player/Player.java
@@ -1,22 +1,52 @@
 package ludum.dare.actors.player;
 
-import com.bitdecay.jump.JumperBody;
+import com.bitdecay.jump.gdx.input.GDXControls;
+import com.bytebreakstudios.animagic.animation.Animation;
+import com.bytebreakstudios.animagic.animation.Animator;
+import com.bytebreakstudios.animagic.animation.FrameRate;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureAtlas;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureRegion;
+import ludum.dare.RacerGame;
 import ludum.dare.actors.StateMachine;
 import ludum.dare.components.*;
 
 public class Player extends StateMachine {
-
-
     public Player() {
-        SizeComponent size = new SizeComponent(1, 2);
+        SizeComponent size = new SizeComponent(100, 100);
         PositionComponent pos = new PositionComponent(0, 0);
-        PhysicsComponent phys = new PhysicsComponent(new JumperBody(), pos, size);
+        //PhysicsComponent phys = new PhysicsComponent(new JumperBody(), pos, size);
         HealthComponent health = new HealthComponent(10, 10);
         AnimationComponent anim = new AnimationComponent("player", pos, size);
-        InputComponent input = new InputComponent(null);
+        setupAnimation(anim.animator);
+//        AnimagicTextureAtlas atlas = RacerGame.assetManager.get("packed/player.atlas", AnimagicTextureAtlas.class);
+//        TextureRegionComponent img = new TextureRegionComponent(atlas.findRegion("run/1"), pos, size);
+        InputComponent input = new InputComponent(new GDXControls());
 
-        this.append(size).append(pos).append(phys).append(health).append(anim).append(input);
+        this.append(size)
+                .append(pos)
+                        //.append(phys)
+                .append(health)
+                .append(anim)
+//                .append(img)
+                .append(input);
 
         this.activeState = new StandState(this.components);
+    }
+
+    private void setupAnimation(Animator a) {
+        AnimagicTextureAtlas atlas = RacerGame.assetManager.get("packed/player.atlas", AnimagicTextureAtlas.class);
+
+        a.addAnimation(new Animation("run", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("run").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("jump", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("jump").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("knockback", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("knockback").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("punch/front", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/front").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("punch/jumping/down", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/jumping/down").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("punch/jumping/front", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/jumping/front").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("punch/jumping/up", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/jumping/up").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("punch/up", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/up").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("stand", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("stand").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("wall", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("wall").toArray(AnimagicTextureRegion.class)));
+
+        a.switchToAnimation("run");
     }
 }
---
+++ src/main/java/ludum/dare/screens/TestStateScreen.java
@@ -0,0 +1,82 @@
+package ludum.dare.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.bytebreakstudios.animagic.texture.AnimagicSpriteBatch;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureAtlas;
+import ludum.dare.RacerGame;
+import ludum.dare.actors.player.Player;
+
+public class TestStateScreen implements Screen {
+
+    OrthographicCamera camera;
+    AnimagicSpriteBatch batch;
+
+    Player player;
+
+    public TestStateScreen(Game game) {
+        super();
+        RacerGame.assetManager.load("packed/player.atlas", AnimagicTextureAtlas.class);
+        RacerGame.queueAssetsForLoad();
+    }
+
+    @Override
+    public void show() {
+        RacerGame.assetManager.finishLoading();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        batch = new AnimagicSpriteBatch(camera);
+        batch.isShaderOn(false);
+
+        player = new Player();
+    }
+
+    @Override
+    public void render(float delta) {
+        player.update(delta);
+
+        camera.update();
+
+        Gdx.gl.glClearColor(0, 0.1f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.setAmbientColor(Color.WHITE);
+        batch.setAmbientIntensity(0.01f);
+        batch.setNextLight(0, 0, 0.1f, 0.9f, Color.WHITE);
+
+        player.draw(batch);
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
