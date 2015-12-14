982c9c898b63f6fcf4880c3a7b012835aad41479
Jacob Fisher <zombiefish90@gmail.com>
2015-12-13 22:42:00 -0700

    asdfsadkjf

--- src/main/java/ludum/dare/components/upgradeComponents/FloatUpgradeComponent.java
+++ src/main/java/ludum/dare/components/upgradeComponents/FloatUpgradeComponent.java
@@ -10,6 +10,6 @@ public class FloatUpgradeComponent implements IComponent {
     public int cost = 0;
 
     public FloatUpgradeComponent(PhysicsComponent phys){
-        phys.getBody().props.gravityModifier = phys.getBody().props.gravityModifier * 0.8qf;
+        phys.getBody().props.gravityModifier = phys.getBody().props.gravityModifier * 0.8f;
     }
 }
