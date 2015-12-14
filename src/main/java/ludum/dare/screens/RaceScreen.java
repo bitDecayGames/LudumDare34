da61553c32f25cbd9efe9ce982750c5e602f6ca7
Michael Wingfield <mwingfie@gmail.com>
2015-12-13 20:00:11 -0700

    ok, actually got the stuff to work, i think

--- src/main/java/ludum/dare/screens/RaceScreen.java
+++ src/main/java/ludum/dare/screens/RaceScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.bitdecay.jump.BitBody;
 import com.bitdecay.jump.BodyType;
 import com.bitdecay.jump.JumperBody;
@@ -24,7 +25,6 @@ import com.bitdecay.jump.level.Level;
 import com.bitdecay.jump.level.LevelObject;
 import com.bitdecay.jump.level.TileObject;
 import com.bitdecay.jump.leveleditor.EditorHook;
-import com.bitdecay.jump.leveleditor.Launcher;
 import com.bitdecay.jump.leveleditor.example.game.SecretObject;
 import com.bitdecay.jump.leveleditor.render.LibGDXWorldRenderer;
 import com.bitdecay.jump.render.JumperRenderStateWatcher;
@@ -42,7 +42,6 @@ import ludum.dare.gameobject.*;
 import ludum.dare.levelobject.*;
 import ludum.dare.levels.LevelSegmentAggregator;
 import ludum.dare.levels.LevelSegmentGenerator;
-import ludum.dare.util.LightUtil;
 import ludum.dare.util.Players;
 import ludum.dare.util.SoundLibrary;
 
@@ -75,10 +74,10 @@ public class RaceScreen implements Screen, EditorHook {
         world.setGravity(0, -700);
 
         AnimagicTextureAtlas atlas = RacerGame.assetManager.get("packed/tiles.atlas", AnimagicTextureAtlas.class);
-        TextureRegion crystalTileTexture = atlas.findRegion("crystal");
-        TextureRegion bridgesTileTexture = atlas.findRegion("bridges");
-        tilesetMap.put(0, crystalTileTexture.split(crystalTileTexture.getRegionWidth() / 16, crystalTileTexture.getRegionHeight())[0]);
-        tilesetMap.put(1, bridgesTileTexture.split(bridgesTileTexture.getRegionWidth() / 16, bridgesTileTexture.getRegionHeight())[0]);
+        Array<AnimagicTextureRegion> crystalTileTextures = atlas.findRegions("crystal");
+        Array<AnimagicTextureRegion> bridgesTileTextures = atlas.findRegions("bridges");
+        tilesetMap.put(0, crystalTileTextures.toArray(TextureRegion.class));
+        tilesetMap.put(1, bridgesTileTextures.toArray(TextureRegion.class));
 
         this.game = game;
         cameras = new OrthographicCamera[Players.list().size()];
@@ -191,7 +190,13 @@ public class RaceScreen implements Screen, EditorHook {
             Gdx.gl.glViewport(0, Gdx.graphics.getHeight() / cameras.length * i, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() / cameras.length);
             OrthographicCamera cam = cameras[i];
             batch.setCamera(cam);
+            batch.setProjectionMatrix(cameras[i].combined);
             batch.begin();
+            Vector3 mousePos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+            batch.setAmbientColor(Color.WHITE);
+            batch.setAmbientIntensity(0.01f);
+            batch.setNextLight(mousePos.x, mousePos.y, 0.1f, 0.9f, Color.WHITE);
+
             drawLevelEdit();
             gameObjects.draw(batch);
             batch.end();
