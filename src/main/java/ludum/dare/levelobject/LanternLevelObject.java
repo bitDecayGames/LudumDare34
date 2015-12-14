c4f730256f11b2889b2705c3e1544d272658a33d
Logan Moore <monday.hopscotch@gmail.com>
2015-12-14 13:31:17 -0700

    allow light modifiers

--- src/main/java/ludum/dare/levelobject/LanternLevelObject.java
+++ src/main/java/ludum/dare/levelobject/LanternLevelObject.java
@@ -3,6 +3,7 @@ package ludum.dare.levelobject;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.bitdecay.jump.BitBody;
 import com.bitdecay.jump.BodyType;
+import com.bitdecay.jump.annotation.ValueRange;
 import com.bitdecay.jump.gdx.level.RenderableLevelObject;
 import com.bitdecay.jump.geom.BitRectangle;
 import com.bytebreakstudios.animagic.texture.AnimagicTextureAtlas;
@@ -13,6 +14,12 @@ import ludum.dare.RacerGame;
  */
 public class LanternLevelObject extends RenderableLevelObject {
     private TextureRegion texture;
+
+    @ValueRange(min = -1, max = 1)
+    public float attenuationModifier = 0;
+
+    @ValueRange(min = -1, max = 1)
+    public float zModifier = 0;
 
     public LanternLevelObject() {
         AnimagicTextureAtlas atlas = RacerGame.assetManager.get("packed/level.atlas", AnimagicTextureAtlas.class);
